f4253f503a978da23400d141ac6e78a1e6b06eee
Logan MacKenzie <logan.mackenzie@geckorobotics.com>
2020-04-25 11:08:59 -0400

    Bumped version to 0.2.2

--- enhanced_versioning/__init__.py
+++ enhanced_versioning/__init__.py
@@ -1,3 +1,3 @@
 from enhanced_versioning.nonsemantic_version import NonSemanticVersion
 from enhanced_versioning.semantic_version import SemanticVersion
-__version__ = str(SemanticVersion('0.2.1'))
+__version__ = str(SemanticVersion('0.2.2'))
